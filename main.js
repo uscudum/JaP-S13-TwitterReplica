d6f47d34825ad3173d88a2986d46ea46a8b25be7
noealvarez93 <noeliaalvareztorres@gmail.com>
2023-09-26 00:16:57 -0300

    formato username + async function

--- main.js
+++ main.js
@@ -1,44 +1,48 @@
-const API = "https://my-json-server.typicode.com/noealvarez93/Twitter/posts";
-let resultado = [];
-const contenedor = document.querySelector('.cont');
+// Solución realizada por: Martin Hegui, Nicolas Rodriguez y Noelia Alvarez
 
+const API_URL = "https://my-json-server.typicode.com/noealvarez93/Twitter/posts";
+const container = document.querySelector('.cont');
 
-document.addEventListener('DOMContentLoaded', function() {
-    fetch(API)
-    .then(response => response.json())
-    .then(result => {
-        resultado = result;
-        mostrarDatos();
+document.addEventListener('DOMContentLoaded', async function () {
+    try {
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+            throw new Error(`Error in request: ${response.status}`);
+        }
 
-    })
-    .catch(error => console.error('Error:', error)); //SE PUEDE PROBAR
+        const data = await response.json();
+        displayData(data);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+});
 
-})
+function displayData(data) {
+    container.innerHTML = ''; 
 
-function mostrarDatos(){
-debugger;
-    resultado.forEach((element, index) => {
-    const html = `
-    <div class="tweets" id="tweets">
-          <div class="profile-pic"><img src="https://picsum.photos/60/60/?random=${index+1}"  style="border-radius: 50%;"/></div>
-          <div class="content">
-            <div class="names">
-              <p class="full-name">${element.name}</p>
+    data.forEach((element, index) => {
+        const formattedUsername = `@${element.name.toLowerCase().replace(/ /g, '_')}`;
+
+        const tweetDiv = document.createElement('div');
+        tweetDiv.classList.add('tweets');
+
+        tweetDiv.innerHTML = `
+            <div class="profile-pic"><img src="https://picsum.photos/60/60/?random=${index + 1}" style="border-radius: 50%;" /></div>
+            <div class="content">
+                <div class="names">
+                    <p class="full-name">${formattedUsername}</p>
+                </div>
             </div>
-          </div>
-          <div class="tweet-content">
-            <p>${element.text}</p>
-          </div>
-          <div class="tweet-icons">
-            <i class="fa fa-comment" aria-hidden="true"></i>
-            <i class="fa fa-heart" aria-hidden="true"></i>
-            <i class="fa fa-retweet" aria-hidden="true"></i>
-          </div>
-          </div>
-    `;
+            <div class="tweet-content">
+                <p>${element.text}</p>
+            </div>
+            <div class="tweet-icons">
+                <i class="fa fa-comment" aria-hidden="true"></i>
+                <i class="fa fa-heart" aria-hidden="true"></i>
+                <i class="fa fa-retweet" aria-hidden="true"></i>
+            </div>
+        `;
 
-        contenedor.innerHTML += html;
-   }); 
-    
-
+        container.appendChild(tweetDiv); 
+    });
 }
